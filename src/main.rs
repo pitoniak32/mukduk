f057d27887b7c57e2cf28453f66bd9ddba2816de
David Pitoniak <davidpitoniak32@gmail.com>
2023-10-16 14:08:08 -0400

    feat: add pretty assertions for tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn should() {
+        assert_eq!(true, false)
+    }
 }
